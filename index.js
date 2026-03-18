ba441e80a37b4931a0dc385102086f654c7c2b26
shivanshsharma10 <outcastedstory@gmail.com>
2026-03-18 14:18:05 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my CI/CD pipeline!');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
