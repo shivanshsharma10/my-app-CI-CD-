2bf2c9a8d2e3ccef3ccd7f75c50c4e7c6734dde9
shivanshsharma10 <outcastedstory@gmail.com>
2026-03-30 21:04:35 +0530

    formating 8

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from my CI/CD pipeline with jenkins yup!');
+  res.send('Hello from my CI/CD pipeline with jenkins yup 2!');
 });
 
 app.listen(PORT, () => {
