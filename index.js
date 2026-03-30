9cea48d42367a4a662685266464a26088f7dddd6
shivanshsharma10 <outcastedstory@gmail.com>
2026-03-30 14:43:07 +0530

    changes in index.js to check pipeline is working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from my CI/CD pipeline with jenkins!');
+  res.send('Hello from my CI/CD pipeline with jenkins yup!');
 });
 
 app.listen(PORT, () => {
